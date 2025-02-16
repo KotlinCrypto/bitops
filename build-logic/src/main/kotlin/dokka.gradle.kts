f2d9b80ffdf6e9f3b82d3132db92164843db25a6
Matthew Nelson <developer@matthewnelson.io>
2025-02-16 06:36:34 -0500

    Add copyright footer (#16)

--- build-logic/src/main/kotlin/dokka.gradle.kts
+++ build-logic/src/main/kotlin/dokka.gradle.kts
@@ -16,6 +16,7 @@
 import org.jetbrains.dokka.gradle.DokkaExtension
 import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
 import java.net.URI
+import java.time.LocalDate
 
 plugins {
     id("org.jetbrains.dokka")
@@ -41,5 +42,9 @@ extensions.configure<DokkaExtension> {
         documentedVisibilities(
             VisibilityModifier.Public,
         )
+    }
+
+    pluginsConfiguration.html {
+        footerMessage.set("© 2025-${LocalDate.now().year} Copyright KotlinCrypto")
     }
 }
